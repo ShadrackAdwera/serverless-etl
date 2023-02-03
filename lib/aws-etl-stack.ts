163aad7ca8b2459622f460133ec0cb477def1447
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-03 17:38:49 +0300

    set up ETL stack - cognito, api gateway, lambda, dynamodb

--- lib/aws-etl-stack.ts
+++ lib/aws-etl-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { FileUploadApiGateway } from './api/file-upload';
+import { CognitoAuthorizer } from './auth/cognito-authorizer';
+import { FileUploadTable } from './database/file-upload-db';
+import { FileUploadLambdaConstruct } from './lambda/file-upload-fn';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsEtlStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsEtlQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const { dynamoFileUploadTable } = new FileUploadTable(
+      this,
+      'dynamodb-file-upload-table'
+    );
+    const { fileUploadFn } = new FileUploadLambdaConstruct(
+      this,
+      'lambda-file-upload-fn',
+      { fileUploadTable: dynamoFileUploadTable }
+    );
+    const { cognitoAuthorizer } = new CognitoAuthorizer(
+      this,
+      'cognito-file-upload-authorizer'
+    );
+    new FileUploadApiGateway(this, 'api-gateway-file-upload', {
+      authorizer: cognitoAuthorizer,
+      fileUploadFn,
+    });
   }
 }
