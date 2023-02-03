9a14af5c808edbeb543445d0812f6125b26455b9
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-03 17:33:10 +0300

    solution construct for dynamodb file upload table

---
+++ lib/database/file-upload-db.ts
@@ -0,0 +1,28 @@
+import { RemovalPolicy } from 'aws-cdk-lib';
+import {
+  AttributeType,
+  BillingMode,
+  ITable,
+  Table,
+} from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+
+export class FileUploadTable extends Construct {
+  public readonly dynamoFileUploadTable: ITable;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    this.dynamoFileUploadTable = this.createFileUploadTable();
+  }
+
+  private createFileUploadTable(): ITable {
+    const fileUploadTable = new Table(this, 'file-upload', {
+      partitionKey: { name: 'id', type: AttributeType['STRING'] },
+      //sortKey: { name: 'dueDate', type: AttributeType['STRING'] },
+      tableName: 'file-uploads',
+      billingMode: BillingMode['PAY_PER_REQUEST'],
+      removalPolicy: RemovalPolicy['DESTROY'],
+    });
+    return fileUploadTable;
+  }
+}
