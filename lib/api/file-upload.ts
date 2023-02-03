5e3b648ebf15c6ea3303ce1d622cc3f1900cfd5b
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-03 17:28:29 +0300

    create POST method construct for file upload lambda fn

---
+++ lib/api/file-upload.ts
@@ -0,0 +1,41 @@
+import {
+  AuthorizationType,
+  IAuthorizer,
+  LambdaIntegration,
+  LambdaRestApi,
+  MethodOptions,
+} from 'aws-cdk-lib/aws-apigateway';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+
+interface IFileUpload {
+  fileUploadFn: IFunction;
+  authorizer: IAuthorizer;
+}
+
+export class FileUploadApiGateway extends Construct {
+  constructor(scope: Construct, id: string, props: IFileUpload) {
+    super(scope, id);
+    this.createFileUploadApis(props.fileUploadFn, props.authorizer);
+  }
+
+  private createFileUploadApis(fileUploadFn: IFunction, auth: IAuthorizer) {
+    const fileUploadApiGateway = new LambdaRestApi(this, 'file-upload-api', {
+      handler: fileUploadFn,
+      restApiName: 'fileUpload-api',
+      proxy: false,
+      description: 'file upload api behind the cognito pool authorizer',
+      deployOptions: {
+        stageName: 'dev',
+      },
+    });
+
+    const httpIntegration = new LambdaIntegration(fileUploadFn);
+    const options: MethodOptions = {
+      authorizer: auth,
+      authorizationType: AuthorizationType['COGNITO'],
+    };
+    const fileUpload = fileUploadApiGateway.root.addResource('fileUpload');
+    fileUpload.addMethod('POST', httpIntegration, options);
+  }
+}
