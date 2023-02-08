809772430d14143bfd91666692d0742be8a87977
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-08 17:30:17 +0300

    bug fix on lambda file upload

--- src/file-upload/index.ts
+++ src/file-upload/index.ts
@@ -1,9 +1,9 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { randomUUID } from 'crypto';
-
+import { createReadStream } from 'fs';
 import { fetchPresignedUrlAndUploadToS3 } from './controllers/file-upload-controllers';
 
-// const bucket = process.env.S3_BUCKET;
+const bucket = process.env.S3_BUCKET;
 
 exports.handler = async (
   event: APIGatewayEvent,
@@ -11,76 +11,65 @@ exports.handler = async (
 ): Promise<APIGatewayProxyResult> => {
   console.log(`Event \n ${JSON.stringify(event, undefined, 2)}`);
   console.log(`Context \n ${JSON.stringify(context, undefined, 2)}`);
-  // if (!bucket) {
-  //   return {
-  //     body: JSON.stringify({ message: 'Configure the S3 BUCKET' }),
-  //     statusCode: 404,
-  //   };
-  // }
+  if (!bucket) {
+    return {
+      body: JSON.stringify({ message: 'Configure the S3 BUCKET' }),
+      statusCode: 404,
+    };
+  }
 
   if (event.httpMethod === 'GET') {
     return {
       body: JSON.stringify({
         message: `GET Method ${randomUUID()}`,
-        encodedCSVFile: process.env.USERPOOL_ID,
-        decodedCSVFile: process.env.USERPOOL_CLIENT_ID,
       }),
       statusCode: 200,
     };
   }
 
   if (event.httpMethod === 'PUT') {
-    if (!event.body) {
-      return {
-        body: JSON.stringify({ message: 'Invalid body' }),
-        statusCode: 400,
-      };
-    }
+    try {
+      if (!event.headers['Content-Type']) {
+        return {
+          body: JSON.stringify({
+            message: 'provide the content-type as multipart/form-data',
+          }),
+          statusCode: 400,
+        };
+      }
+      if (!event.body) {
+        return {
+          body: JSON.stringify({ message: 'provide the file' }),
+          statusCode: 400,
+        };
+      }
+      const data = Buffer.from(event.body, 'utf-8');
+      console.log(`Read File: ${data}`);
 
-    let encodedCSVFile = JSON.parse(event.body).file;
-    if (!encodedCSVFile) {
+      if (!data) {
+        return {
+          body: JSON.stringify({ message: 'cannot read file contents' }),
+          statusCode: 400,
+        };
+      }
+      const res = await fetchPresignedUrlAndUploadToS3({
+        Bucket: bucket,
+        Body: data,
+        Key: `${randomUUID()}.csv`,
+      });
       return {
-        body: JSON.stringify({ message: 'Provide a file' }),
+        body: JSON.stringify({
+          message: 'File uplaoded successfully to your S3 bucket',
+        }),
+        statusCode: 201,
+      };
+    } catch (err) {
+      return {
+        body: JSON.stringify({ message: err }),
         statusCode: 400,
       };
     }
-    let decodedCSVFile = Buffer.from(encodedCSVFile, 'utf-8');
-    return {
-      body: JSON.stringify({
-        message: `PUT Method`,
-        encodedCSVFile,
-        decodedCSVFile,
-      }),
-      statusCode: 200,
-    };
   }
-
-  // if (event.httpMethod === 'PUT') {
-  //   try {
-  //     const res = await fetchPresignedUrlAndUploadToS3({
-  //       Bucket: bucket,
-  //       Body: '',
-  //       Key: randomUUID(),
-  //     });
-  //     return {
-  //       body: JSON.stringify({
-  //         message: 'File uplaoded successfully to your S3 bucket',
-  //         data: res,
-  //       }),
-  //       statusCode: 201,
-  //     };
-  //   } catch (err) {
-  //     return {
-  //       body: JSON.stringify({ message: err }),
-  //       statusCode: 400,
-  //     };
-  //   }
-  // } else {
-  //   return {
-  //     body: JSON.stringify({ message: 'Invalid method ...' }),
-  //     statusCode: 404,
-  //   };
-  // }
   return {
     body: JSON.stringify({ message: 'Invalid method ...' }),
     statusCode: 404,
