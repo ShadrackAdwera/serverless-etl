08a9bf696116fd08082c04a033415ee4db10a375
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-09 10:15:59 +0300

    decode cognito auth token

--- src/file-upload/index.ts
+++ src/file-upload/index.ts
@@ -1,7 +1,12 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { randomUUID } from 'crypto';
-import { createReadStream } from 'fs';
-import { fetchPresignedUrlAndUploadToS3 } from './controllers/file-upload-controllers';
+import { CognitoJwtVerifier } from 'aws-jwt-verify';
+
+import {
+  fetchPresignedUrlAndUploadToS3,
+  sendFileUrlToDynamoDB,
+} from './controllers/file-upload-controllers';
+import { CognitoIdTokenPayload } from 'aws-jwt-verify/jwt-model';
 
 const bucket = process.env.S3_BUCKET;
 
@@ -17,6 +22,33 @@ exports.handler = async (
       statusCode: 404,
     };
   }
+
+  if (!process.env.USERPOOL_ID) {
+    return {
+      body: JSON.stringify({ message: 'Invalid User Pool ID configuration' }),
+      statusCode: 403,
+    };
+  }
+
+  if (!process.env.USERPOOL_CLIENT_ID) {
+    return {
+      body: JSON.stringify({
+        message: 'Invalid User Pool Client ID configuration',
+      }),
+      statusCode: 403,
+    };
+  }
+
+  const jwt = event.headers['Authorization'];
+
+  if (!jwt) {
+    return {
+      body: JSON.stringify({ message: 'Cannot retreive JWT' }),
+      statusCode: 400,
+    };
+  }
+
+  const payload = await decodeCogitoPayload(jwt);
 
   if (event.httpMethod === 'GET') {
     return {
@@ -45,6 +77,7 @@ exports.handler = async (
       }
       const data = Buffer.from(event.body, 'utf-8');
       console.log(`Read File: ${data}`);
+      const key = `${payload['cognito:username']}/${randomUUID()}.csv`;
 
       if (!data) {
         return {
@@ -52,14 +85,18 @@ exports.handler = async (
           statusCode: 400,
         };
       }
-      const res = await fetchPresignedUrlAndUploadToS3({
+      await fetchPresignedUrlAndUploadToS3({
         Bucket: bucket,
         Body: data,
-        Key: `${randomUUID()}.csv`,
+        Key: key,
+      });
+      await sendFileUrlToDynamoDB({
+        fileUrl: key,
+        userId: payload['cognito:username'],
       });
       return {
         body: JSON.stringify({
-          message: 'File uplaoded successfully to your S3 bucket',
+          message: 'File uploaded successfully to your S3 bucket',
         }),
         statusCode: 201,
       };
@@ -74,4 +111,21 @@ exports.handler = async (
     body: JSON.stringify({ message: 'Invalid method ...' }),
     statusCode: 404,
   };
+};
+
+const decodeCogitoPayload = async (
+  jwt: string
+): Promise<CognitoIdTokenPayload> => {
+  try {
+    const verifier = CognitoJwtVerifier.create({
+      userPoolId: process.env.USERPOOL_ID!,
+      tokenUse: 'id',
+      clientId: process.env.USERPOOL_CLIENT_ID!,
+    });
+
+    const payload = await verifier.verify(jwt);
+    return payload;
+  } catch (error) {
+    throw error;
+  }
 };
