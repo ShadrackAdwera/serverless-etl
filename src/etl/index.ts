ffab668c9d07666536a35152bd970381bcbbc189
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-10 08:45:20 +0300

    set up event stream body

--- src/etl/index.ts
+++ src/etl/index.ts
@@ -13,3 +13,43 @@ exports.handler = async (event: DynamoDBStreamEvent, context: Context) => {
     return;
   }
 };
+
+/*
+{
+    "Records": [
+        {
+            "eventID": "**",
+            "eventName": "INSERT",
+            "eventVersion": "1.1",
+            "eventSource": "aws:dynamodb",
+            "awsRegion": "us-east-1",
+            "dynamodb": {
+                "ApproximateCreationDateTime": 1676007513,
+                "Keys": {
+                    "id": {
+                        "S": "7f853885-dda6-4e48-b970-dba1ecac0258"
+                    }
+                },
+                "NewImage": {
+                    "createdAt": {
+                        "S": "2023-02-10T05:38:32.757Z"
+                    },
+                    "fileUrl": {
+                        "S": "b6353c46-457c-45e6-ac53-52e169cd2ae2/09367079-dc4d-4f97-9bdf-05dea69d8112.csv"
+                    },
+                    "id": {
+                        "S": "7f853885-dda6-4e48-b970-dba1ecac0258"
+                    },
+                    "userId": {
+                        "S": "b6353c46-457c-45e6-ac53-52e169cd2ae2"
+                    }
+                },
+                "SequenceNumber": "100000000012912323457",
+                "SizeBytes": 235,
+                "StreamViewType": "NEW_AND_OLD_IMAGES"
+            },
+            "eventSourceARN": "**"
+        }
+    ]
+}
+*/
