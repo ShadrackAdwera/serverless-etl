fd25931f8b1927701627dbaef63effd023b38b0c
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-16 08:32:05 +0300

    set up event Bridge config

---
+++ lib/events/eventbus.ts
@@ -0,0 +1,57 @@
+import { EventBus, IEventBus, Rule } from 'aws-cdk-lib/aws-events';
+import { SqsQueue } from 'aws-cdk-lib/aws-events-targets';
+import { IQueue } from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+export const EVENT_SOURCE = 'com.serverless.etl.items';
+export const EVENT_DETAILTYPE = 'EtlToSqlsItems';
+export const EVENT_BUSNAME = 'EtlToSqlsEventBus';
+
+interface IEventBusProps {
+  target: IQueue;
+}
+
+export class EtlToSqlsEventBus extends Construct {
+  constructor(scope: Construct, id: string, props: IEventBusProps) {
+    super(scope, id);
+    // event bus
+    const bus = this.generateEventBus();
+    // rule
+    const etlToSqsBasketRule = this.generateRule(bus);
+    etlToSqsBasketRule.addTarget(new SqsQueue(props.target));
+  }
+
+  private generateEventBus(): IEventBus {
+    const eventBus = new EventBus(this, 'EtlToSqlsEventBus', {
+      eventBusName: EVENT_BUSNAME,
+    });
+    return eventBus;
+  }
+
+  private generateRule(bus: IEventBus): Rule {
+    const rule = new Rule(this, 'EtlToSqlsRule', {
+      eventBus: bus,
+      enabled: true,
+      description: 'Handle ETL events from etl microservice',
+      eventPattern: {
+        source: [EVENT_SOURCE],
+        detailType: [EVENT_DETAILTYPE],
+      },
+      ruleName: 'EtlToSqlsRule',
+    });
+    return rule;
+  }
+}
+
+/**
+ * SAMPLE EVENT
+ * [
+  { "Source": "com.serverless.checkout.items", 
+  "Detail": "{ "username": "abc", "item": "item 1" }", 
+  "Resources": ["resource1","resource2"], 
+  "DetailType": "CheckoutItems", 
+  "EventBusName":"OrdersCheckoutEventBus" 
+}
+  ]
+ * 
+ */
