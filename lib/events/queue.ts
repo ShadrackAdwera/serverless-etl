16fa8b0ce288791921b2acc7d7e13bc3b2929fa9
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-16 08:32:25 +0300

    set up queue target for Event Bus

---
+++ lib/events/queue.ts
@@ -0,0 +1,25 @@
+import { Duration } from 'aws-cdk-lib';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { Queue, IQueue } from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+interface QueueProps {
+  consumer: IFunction;
+}
+
+export class EtlQueue extends Construct {
+  public readonly etlQueue: IQueue;
+  constructor(scope: Construct, id: string, props: QueueProps) {
+    super(scope, id);
+    this.etlQueue = new Queue(this, 'EtlQueue', {
+      queueName: 'EtlQueue',
+      visibilityTimeout: Duration.seconds(30),
+    });
+    props.consumer.addEventSource(
+      new SqsEventSource(this.etlQueue, {
+        batchSize: 1,
+      })
+    );
+  }
+}
